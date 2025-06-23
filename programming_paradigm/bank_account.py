2a1132cfca869a4c63047b3a60fe072c75bc200a
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2025-06-23 16:15:48 +0000

    banking app

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+        print(f"Deposited: ${self.account_balance}")
+        
+    
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            print(f"Withdrew: ${self.account_balance}")
+        else:
+            print("Insufficient funds.")
+
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
