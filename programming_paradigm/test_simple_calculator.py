4958631ba2f946228a5d40083fe3311872d432ac
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2025-06-23 16:41:53 +0000

    testing

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,37 +10,24 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(-5, -5), -10)
-        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
-        """Test the subtraction method."""
-        self.assertEqual(self.calc.subtract(10, 4), 6)
-        self.assertEqual(self.calc.subtract(0, 5), -5)
-        self.assertEqual(self.calc.subtract(-3, -3), 0)
-        self.assertEqual(self.calc.subtract(-5, 3), -8)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
 
     def test_multiply(self):
-        """Test the multiplication method."""
-        self.assertEqual(self.calc.multiply(2, 3), 6)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 1000), 0)
-        self.assertEqual(self.calc.multiply(-3, -3), 9)
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-1, 3), -3)
 
     def test_divide(self):
-        """Test the division method."""
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-9, 3), -3)
-        self.assertEqual(self.calc.divide(-8, -2), 4)
+        self.assertEqual(self.calc.divide(10, 2), 5.0)
         self.assertAlmostEqual(self.calc.divide(1, 3), 0.3333, places=4)
 
     def test_divide_by_zero(self):
-        """Test division by zero raises ZeroDivisionError."""
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(5, 0)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
